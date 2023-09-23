089599af47770f48f36c501e815377ce32e26e66
Leandro Norcio <onsongnorcio@gmail.com>
2023-09-24 01:34:30 +0800

    Added a more user-friendly error message when the user tries to load a non-existent contact.

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -3,6 +3,12 @@ import { getContact, updateContact } from '../contacts';
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response('', {
+      status: 404,
+      statusText: 'This contact does not exist.',
+    });
+  }
   return { contact };
 }
 
